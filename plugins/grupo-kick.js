5e78124e9618f94ef039e0794098573db19805b3
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-10-13 20:02:47 -0500

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,6 +1,6 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, `${emojis} Debes mencionar a un usuario para poder expulsarlo del grupo.`, m, rcanal);
+        return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder expulsarlo del grupo.`, m);
     }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
@@ -11,15 +11,15 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     //const nn = conn.getName(m.sender);
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar el bot del grupo.`, m, rcanal);
+        return conn.reply(m.chat, `${emoji2} No puedo eliminar el bot del grupo.`, m);
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del grupo.`, m, rcanal);
+        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del grupo.`, m);
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del bot.`, m, rcanal);
+        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del bot.`, m);
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
@@ -29,7 +29,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
 handler.help = ['kick'];
 handler.tags = ['grupo'];
-handler.command = ['kick', 'echar', 'hechar', 'sacar', 'ban'];
+handler.command = ['kick','echar','hechar','sacar','ban'];
 handler.admin = true;
 handler.group = true;
 handler.register = true
